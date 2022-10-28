950ee2bf9b1e71f5956a2809ebec48c8492be31d
Awesome Stickz <kiran121102@gmail.com>
2022-10-28 22:06:58 +0530

    Improve code

--- setupCacheEdits.ts
+++ setupCacheEdits.ts
@@ -5,38 +5,30 @@ import { BotWithProxyCache, ProxyCacheTypes } from './index.js';
 export function setupCacheEdits<B extends Bot>(bot: BotWithProxyCache<ProxyCacheTypes, B>) {
     const { GUILD_MEMBER_ADD, GUILD_MEMBER_REMOVE } = bot.handlers;
 
-    bot.handlers.GUILD_MEMBER_ADD = function (_, data, shardId) {
+    bot.handlers.GUILD_MEMBER_ADD = async function (_, data, shardId) {
         const payload = data.d as DiscordGuildMemberAdd;
 
         const guildID = bot.transformers.snowflake(payload.guild_id);
         const guild = bot.cache.guilds.memory.get(guildID);
-        // Update in memory cache
-        if (guild) guild.memberCount++;
-        // Update non-memory cache
-        bot.cache.guilds.get(guildID).then((g) => {
-            if (g) {
-                g.memberCount++;
-                bot.cache.guilds.set(g);
-            }
-        });
+
+        if (guild) {
+            guild.memberCount++;
+            await bot.cache.guilds.set(guild);
+        }
 
         GUILD_MEMBER_ADD(bot, data, shardId);
     };
 
-    bot.handlers.GUILD_MEMBER_REMOVE = function (_, data, shardId) {
+    bot.handlers.GUILD_MEMBER_REMOVE = async function (_, data, shardId) {
         const payload = data.d as DiscordGuildMemberRemove;
 
         const guildID = bot.transformers.snowflake(payload.guild_id);
         const guild = bot.cache.guilds.memory.get(guildID);
-        // Update in memory cache
-        if (guild) guild.memberCount--;
-        // Update non-memory cache
-        bot.cache.guilds.get(guildID).then((g) => {
-            if (g) {
-                g.memberCount--;
-                bot.cache.guilds.set(g);
-            }
-        });
+
+        if (guild) {
+            guild.memberCount--;
+            await bot.cache.guilds.set(guild);
+        }
 
         GUILD_MEMBER_REMOVE(bot, data, shardId);
     };
