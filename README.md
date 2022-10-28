68486666fbd2384f059d967e446d885d985d9355
Awesome Stickz <kiran121102@gmail.com>
2022-10-28 22:05:04 +0530

    Move to AwesomeStickz/dd-cache-proxy

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# cache-proxy-plugin
+# dd-cache-proxy
 
-This plugin adds support for caching for your bot. It provides two types of cache.
-
-1. In Memory Cache (Crazy fast but lost whenever your bot restarts.)
-2. Persistent Cache (Crazy fast but slower than memory. Not lost unless you restart your cache.)
-3. 
+The cache proxy in discordeno that's used in [Giveaway Boat](https://giveaway.boats). Forked from [cache-proxy](https://github.com/discordeno/cache-proxy)
--- setupCacheEdits.ts
+++ setupCacheEdits.ts
@@ -1,48 +1,43 @@
-import { Bot } from "discordeno";
-import {
-  DiscordGuildMemberAdd,
-  DiscordGuildMemberRemove,
-} from "discordeno/types";
-import { BotWithProxyCache, ProxyCacheTypes } from "./index.js";
+import { Bot } from 'discordeno';
+import { DiscordGuildMemberAdd, DiscordGuildMemberRemove } from 'discordeno/types';
+import { BotWithProxyCache, ProxyCacheTypes } from './index.js';
 
-export function setupCacheEdits<B extends Bot>(
-  bot: BotWithProxyCache<ProxyCacheTypes, B>
-) {
-  const { GUILD_MEMBER_ADD, GUILD_MEMBER_REMOVE } = bot.handlers;
+export function setupCacheEdits<B extends Bot>(bot: BotWithProxyCache<ProxyCacheTypes, B>) {
+    const { GUILD_MEMBER_ADD, GUILD_MEMBER_REMOVE } = bot.handlers;
 
-  bot.handlers.GUILD_MEMBER_ADD = function (_, data, shardId) {
-    const payload = data.d as DiscordGuildMemberAdd;
+    bot.handlers.GUILD_MEMBER_ADD = function (_, data, shardId) {
+        const payload = data.d as DiscordGuildMemberAdd;
 
-    const guildID = bot.transformers.snowflake(payload.guild_id);
-    const guild = bot.cache.guilds.memory.get(guildID);
-    // Update in memory cache
-    if (guild) guild.memberCount++;
-    // Update non-memory cache
-    bot.cache.guilds.get(guildID).then((g) => {
-      if (g) {
-        g.memberCount++;
-        bot.cache.guilds.set(g);
-      }
-    });
+        const guildID = bot.transformers.snowflake(payload.guild_id);
+        const guild = bot.cache.guilds.memory.get(guildID);
+        // Update in memory cache
+        if (guild) guild.memberCount++;
+        // Update non-memory cache
+        bot.cache.guilds.get(guildID).then((g) => {
+            if (g) {
+                g.memberCount++;
+                bot.cache.guilds.set(g);
+            }
+        });
 
-    GUILD_MEMBER_ADD(bot, data, shardId);
-  };
+        GUILD_MEMBER_ADD(bot, data, shardId);
+    };
 
-  bot.handlers.GUILD_MEMBER_REMOVE = function (_, data, shardId) {
-    const payload = data.d as DiscordGuildMemberRemove;
+    bot.handlers.GUILD_MEMBER_REMOVE = function (_, data, shardId) {
+        const payload = data.d as DiscordGuildMemberRemove;
 
-    const guildID = bot.transformers.snowflake(payload.guild_id);
-    const guild = bot.cache.guilds.memory.get(guildID);
-    // Update in memory cache
-    if (guild) guild.memberCount--;
-    // Update non-memory cache
-    bot.cache.guilds.get(guildID).then((g) => {
-      if (g) {
-        g.memberCount--;
-        bot.cache.guilds.set(g);
-      }
-    });
+        const guildID = bot.transformers.snowflake(payload.guild_id);
+        const guild = bot.cache.guilds.memory.get(guildID);
+        // Update in memory cache
+        if (guild) guild.memberCount--;
+        // Update non-memory cache
+        bot.cache.guilds.get(guildID).then((g) => {
+            if (g) {
+                g.memberCount--;
+                bot.cache.guilds.set(g);
+            }
+        });
 
-    GUILD_MEMBER_REMOVE(bot, data, shardId);
-  };
+        GUILD_MEMBER_REMOVE(bot, data, shardId);
+    };
 }
